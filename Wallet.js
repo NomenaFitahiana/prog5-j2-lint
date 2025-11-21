798730f839a62f7e11ce4ebff09c5cd6c01b87d3
NomenaFitahiana <nomenafitahiana.randrianirina@gmail.com>
2025-11-21 16:53:04 +0300

    create wallet class

---
+++ Wallet.js
@@ -0,0 +1,78 @@
+class Wallet{
+    #color;
+    #size;
+    #isLost;
+    #isOpen;
+    #totalMoney;
+
+
+    constructor(color, size, totalMoney = 0){
+        this.#color = color;
+        this.#size = size;
+        this.#isLost = false;
+        this.#isOpen = false;
+        this.#totalMoney = totalMoney;
+    }
+
+    get color(){
+        return this.#color;
+    }
+    
+    get size(){
+        return this.#size;
+    }
+
+    get isLost(){
+        return this.#isLost;
+    }
+    
+    get isOpen(){
+        return this.#isOpen;
+    }
+
+    get totalMoney(){
+        return this.#totalMoney;
+    }
+
+    set color(newColor){
+        this.#color = newColor;
+    }
+
+    set size(newSize){
+        this.#size = newSize;
+    }
+
+    set totalMoney(amount){
+        this.#totalMoney = amount;
+    }
+
+    toggleLost(){
+        this.#isLost = !this.#isLost;
+        return `Wallet lost status: ${this.#isLost}`;
+    }
+
+    addMoney(newAmount){
+        this.#totalMoney +=  newAmount;
+        return this.#totalMoney;
+    }
+
+    getMoney(amount){
+        this.#totalMoney -= amount;
+        return this.#totalMoney;
+    }
+
+    checkTotalMoney(){
+        return `Total money in wallet: $${this.#totalMoney}`;
+    }
+
+
+    changeStatus(){
+        this.#isOpen = !this.#isOpen;
+
+        return `Wallet is now ${this.#isOpen ? 'open' : 'closed'}.`;
+    }
+}
+
+let myWallet = new Wallet('black', 'medium', 100);
+
+console.log(myWallet.checkTotalMoney());
--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,10 @@ import js from "@eslint/js";
 
 export default defineConfig([
 	{
+		env: {
+      	node: true,       
+      	es2021: true
+    	},
 		files: ["**/*.js"],
 		plugins: {
 			js,
@@ -11,6 +15,23 @@ export default defineConfig([
 		rules: {
 			"no-unused-vars": "error",
 			"no-undef": "error",
+			"camelcase": ["error", {
+				properties: "always",
+				ignoreDestructuring: false
+			}],
+			"new-cap": ["error", {
+				newIsCap: true,
+				capIsNew: false
+			}],
+			"id-match": ["error",
+        "^[a-z][a-zA-Z0-9]*$|^[A-Z][a-zA-Z0-9]*$",
+        {
+        	onlyDeclarations: true,
+          	properties: false,
+          	classFields: false,
+          	ignoreDestructuring: false
+        }
+			],
 		},
 	},
 ]);
